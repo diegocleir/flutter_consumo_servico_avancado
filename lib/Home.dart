eb23a97e20f3bc5b2d6830b70653edf0faacbaa1
Diego Alves <diegocleir@gmail.com>
2022-10-04 10:41:12 -0300

    melhorias no projeto

--- lib/Home.dart
+++ lib/Home.dart
@@ -34,52 +34,77 @@ class _HomeState extends State<Home> {
 
   }
 
+  _post(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviço anvançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot){
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: _post,
+                    child: Text("Salvar")
+                )
+              ],
+            ),
 
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.active:
-            case ConnectionState.done:
-              //print("conexao done");
-              if(snapshot.hasError){
-                //print("lista: Erro ao carregar ");
+            FutureBuilder<List<Post>>(
+              future: _recuperarPostagens(),
+              builder: (context, snapshot){
 
-                return Text("lista: Erro ao carregar ");
-
-              } else {
-
-                //print("lista: carregou!! ");
-
-                return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index) {
-
-                    List<Post>? lista = snapshot.data;
-                    Post? post = lista?[index];
-
-                    return ListTile(
-                      title: Text(post?.title),
-                      subtitle: Text(post?.id.toString()),
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                    return Text("");
+                    break;
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
                     );
-                  }
-                );
-              }
-              break;
-          }
-        },
+                    break;
+                  case ConnectionState.active:
+                    return Text("");
+                    break;
+                  case ConnectionState.done:
+                  //print("conexao done");
+                    if(snapshot.hasError){
+                      //print("lista: Erro ao carregar ");
+
+                      return Text("lista: Erro ao carregar ");
+
+                    } else {
+
+                      //print("lista: carregou!! ");
+
+                      List<Post> lista = snapshot.data ?? [];
+
+                      return ListView.builder(
+                          itemCount: lista.length,
+                          itemBuilder: (context, index) {
+
+                            Post post = lista[index];
+
+                            return ListTile(
+                              title: Text(post.title),
+                              subtitle: Text(post.id.toString()),
+                            );
+                          }
+                      );
+                    }
+                    break;
+                }
+              },
+            )
+
+          ],
+        ),
       ),
     );
   }
